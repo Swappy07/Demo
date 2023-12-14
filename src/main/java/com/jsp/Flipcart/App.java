8cb0710ae1332e57715cc12bfe9d02397b964ee1
Swappy07 <swappy4956@gmail.com>
2023-12-14 13:44:07 +0530

    changed

---
+++ src/main/java/com/jsp/Flipcart/App.java
@@ -0,0 +1,14 @@
+package com.jsp.Flipcart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    // cmd common
+}
